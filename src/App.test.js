02ea0d5e67280d58dd5b2a1eff4be3d5125935fe
Michael Duberstein <dubermic@flutterint.com>
2022-10-17 13:19:50 -0400

    After Clip 14: LogRoles method for debugging roles

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import {logRoles} from '@testing-library/react';
+// import {logRoles} from '@testing-library/react';
 
 // find an element with a role of button and text of 'Change to blue'
 test('button has correct initial color', () => {
-  const {container} = render(<App />);
-  logRoles(container);
+  // const {container} = render(<App />);
+  // logRoles(container);
+  render(<App />)
 
   const colorButton = screen.getByRole('button', {name:'Change to blue' }); 
   // expect the background color to be red
