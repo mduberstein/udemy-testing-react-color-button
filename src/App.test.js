12b8653ffcc3db81ecdd97f2f842cd220b819a56
Michael Duberstein <dubermic@flutterint.com>
2022-10-17 11:02:24 -0400

    After Clip 5 Jest: Watch Mode and How Tests Work, failing the test example

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+  throw new Error('Test failed');
+  // render(<App />);
+  // const linkElement = screen.getByText('Learn React');
+  // expect(linkElement).toBeInTheDocument();
 });
