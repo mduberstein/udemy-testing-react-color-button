102539bee1d0932d1e5b17569a2bd220607741e2
Michael Duberstein <dubermic@flutterint.com>
2022-10-17 13:59:56 -0400

    After Clip 15 Test Behavior when Clicking Button

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 // import {logRoles} from '@testing-library/react';
 
 // find an element with a role of button and text of 'Change to blue'
-test('button has correct initial color', () => {
+test('button has correct initial color and updates when clicked', () => {
   // const {container} = render(<App />);
   // logRoles(container);
   render(<App />)
@@ -11,11 +11,19 @@ test('button has correct initial color', () => {
   const colorButton = screen.getByRole('button', {name:'Change to blue' }); 
   // expect the background color to be red
   expect(colorButton).toHaveStyle({backgroundColor: 'red'});
+
+  // click button
+  fireEvent.click(colorButton);
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({backgroundColor: 'blue'});
+  // expect the text to be 'Change to red'
+  expect(colorButton).toHaveTextContent('Change to red');
 });
 
-test('button turns blue when clicked', () => {
-
-});
+// test('button turns blue when clicked', () => {
+//   render(<App />);
+//   const colorButton = screen.getByRole('button', {name:'Change to blue' });
+// });
 
 // test('renders learn react link', () => {
 //   // throw new Error('Test failed');
